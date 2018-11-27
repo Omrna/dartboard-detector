219aaa6e1ed62552f9c5ee60c880cbc3a3bf18da
Faizaan Sakib <ss16161@it025974.users.bris.ac.uk>
2018-11-27 15:37:23 +0000

    F1 score implemented - subtask 1 COMPLETE

--- draw.cpp
+++ draw.cpp
@@ -14,18 +14,8 @@ int main(int argc, const char** argv) {
 
   Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-  rectangle(frame, Point(65, 135), Point(120, 210), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(250, 160), Point(310, 225), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(375, 185), Point(440, 245), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(515, 180), Point(575, 245), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(645, 185), Point(705, 245), Scalar( 0, 0, 255 ), 2);
-
-  rectangle(frame, Point(55, 250), Point(115, 315), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(195, 215), Point(250, 285), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(285, 240), Point(350, 310), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(430, 235), Point(485, 305), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(560, 250), Point(620, 315), Scalar( 0, 0, 255 ), 2);
-  rectangle(frame, Point(675, 250), Point(735, 315), Scalar( 0, 0, 255 ), 2);
+  rectangle(frame, Point(460, 220), Point(555, 320), Scalar( 0, 0, 255 ), 2);
+  rectangle(frame, Point(730, 190), Point(830, 295), Scalar( 0, 0, 255 ), 2);
 
   imwrite( "annotated.jpg", frame );
 
--- face.cpp
+++ face.cpp
@@ -13,6 +13,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <string>
 #include <iomanip>
 #include <fstream>
 #include <sstream>
@@ -22,18 +23,16 @@
 using namespace std;
 using namespace cv;
 
-
-
 /** Function Headers */
 void detectAndDisplay( Mat frame );
-void F1Test( int facesDetected, const char* imgName );
+float F1Test( int facesDetected, const char* imgName, Mat frame );
 
 /** Global variables */
 String cascade_name = "frontalface.xml";
 CascadeClassifier cascade;
-std::vector<Rect> faces;
+std::vector<Rect> detectedFaces;
+std::vector<Rect> trueFaces;
 
-/** @function main */
 int main( int argc, const char** argv )
 {
 
@@ -49,7 +48,7 @@ int main( int argc, const char** argv )
 	detectAndDisplay( frame );
 
 	// ADDED: 4. Perform F1 test
-	F1Test(faces.size(), imgName);
+	float f1score = F1Test(detectedFaces.size(), imgName, frame);
 
 	// 5. Save Result Image
 	imwrite( "detected.jpg", frame );
@@ -57,9 +56,10 @@ int main( int argc, const char** argv )
 	return 0;
 }
 
-void F1Test( int facesDetected, const char* imgName ){
-	int truePositives = 0;
+float F1Test( int facesDetected, const char* imgName, Mat frame ){
+	int validFaces = 0;
 
+	// Manipulate string to get correct CSV file name
 	string fileExtension = "points.csv";
 	string current_line;
 
@@ -70,27 +70,74 @@ void F1Test( int facesDetected, const char* imgName ){
 		imgNameString.replace(i, fileExtension.length(), fileExtension);
   }
 
-	std::cout << imgNameString << " \n";
-
 	const char *c = imgNameString.c_str();
-
 	ifstream inputFile(c);
 
+	// Go through CSV file line by line
 	while(getline(inputFile, current_line)){
-   ++truePositives;
+
+		// Array of values for each rectangle
+		std::vector<int> values;
+
+		std::stringstream convertor(current_line);
+		std::string token; // somewhere to put the comma separated value
+
+		// Insert each value into values array
+		while (std::getline(convertor, token, ',')) {
+			values.push_back(std::atoi(token.c_str()));
+		}
+
+		// Populate array with ground truth rectangles
+		trueFaces.push_back(Rect(values[0], values[1], values[2], values[3]));
 	}
 
-	std::cout << truePositives << "\n";
+	int truePositives = 0;
+	int falsePositives = 0;
 
-	// float F1Test =
-	// Use rectangle intersection Function
+	// Compare each detected face to every ground truth face
+	for (int i = 0; i < detectedFaces.size(); i++) {
+		for (int j = 0; j < trueFaces.size(); j++) {
+			// Get intersection and check matching area percentage
+			Rect intersection = detectedFaces[i] & trueFaces[j];
+			float intersectionArea = intersection.area();
+
+			// If there is an intersection, check percentage of intersection area
+			// to detection area
+			if (intersectionArea > 0) {
+				float matchPercentage = (intersectionArea / trueFaces[j].area()) * 100;
+
+				// If threshold reached, increment true positives
+				if (matchPercentage > 60){
+					truePositives++;
+					break;
+				}
+				if (j == (trueFaces.size() - 1)) falsePositives++;
+			}
+			// If loop reaches end without reaching intersection threshold, it is
+			// a false negative
+			else {
+				if (j == (trueFaces.size() - 1)) falsePositives++;
+			}
+		}
+	}
+
+	std::cout << "true positives: " << truePositives << ", false positives: " << falsePositives << "\n";
+
+	// Time for F1 test
 	// Precision = TP / (TP + FP)
-	// Recall = TPR
+	// Recall = TPR (True Positive Rate)
 	// F1 = 2((PRE * REC)/(PRE + REC))
 
+	float precision = (float)truePositives / ((float)truePositives + (float)falsePositives);
+	float recall = (float)truePositives / (float)trueFaces.size();
+
+	float f1 = 2 * ((precision * recall)/(precision + recall));
+
+	std::cout << "f1 score: " << f1 << "\n";
+
+	return f1;
 }
 
-/** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
 	Mat frame_gray;
@@ -100,15 +147,15 @@ void detectAndDisplay( Mat frame )
 	equalizeHist( frame_gray, frame_gray );
 
 	// 2. Perform Viola-Jones Object Detection
-	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+	cascade.detectMultiScale( frame_gray, detectedFaces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
-       // 3. Print number of Faces found
-	std::cout << "faces detected: " << faces.size() << std::endl;
+  // 3. Print number of Faces found
+	std::cout << "faces detected: " << detectedFaces.size() << std::endl;
 
-       // 4. Draw box around faces found
-	for( int i = 0; i < faces.size(); i++ )
+  // 4. Draw box around faces found
+	for( int i = 0; i < detectedFaces.size(); i++ )
 	{
-		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+		rectangle(frame, Point(detectedFaces[i].x, detectedFaces[i].y), Point(detectedFaces[i].x + detectedFaces[i].width, detectedFaces[i].y + detectedFaces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
 
 }
